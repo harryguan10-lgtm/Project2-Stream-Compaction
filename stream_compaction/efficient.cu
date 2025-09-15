1d9d91845d834e3b722262db3b56b1c6acdc326c
Guan <harryg1@kite.upenn.edu>
2025-09-14 21:48:56 -0400

    feat: did part 3.2

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -98,9 +98,52 @@ namespace StreamCompaction {
          */
         int compact(int n, int *odata, const int *idata) {
             timer().startGpuTimer();
-            // TODO
+
+            int* dev_idata, * dev_bools, * dev_indices, * dev_odata;
+            cudaMalloc((void**)&dev_idata, n * sizeof(int));
+            checkCUDAError("cudaMalloc dev_idata failed!");
+            cudaMalloc((void**)&dev_bools, n * sizeof(int));
+            checkCUDAError("cudaMalloc dev_bools failed!");
+            cudaMalloc((void**)&dev_indices, n * sizeof(int));
+            checkCUDAError("cudaMalloc dev_indices failed!");
+            cudaMalloc((void**)&dev_odata, n * sizeof(int));
+            checkCUDAError("cudaMalloc dev_odata failed!");
+
+            cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy to device failed!");
+
+            const int blockSize = 128;
+            dim3 blockDim(blockSize);
+            dim3 gridDim((n + blockSize - 1) / blockSize);
+
+            StreamCompaction::Common::kernMapToBoolean << <gridDim, blockDim >> > (n, dev_bools, dev_idata);
+            checkCUDAError("kernMapToBoolean failed!");
+
+            int* host_bools = new int[n];
+            int* host_indices = new int[n];
+            cudaMemcpy(host_bools, dev_bools, n * sizeof(int), cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy bools to host failed!");
+
+            scan(n, host_indices, host_bools);
+
+            cudaMemcpy(dev_indices, host_indices, n * sizeof(int), cudaMemcpyHostToDevice);
+            checkCUDAError("cudaMemcpy indices to device failed!");
+
+            StreamCompaction::Common::kernScatter << <gridDim, blockDim >> > (n, dev_odata, dev_idata, dev_bools, dev_indices);
+            checkCUDAError("kernScatter failed!");
+
+            int count = host_indices[n - 1] + host_bools[n - 1];
+
+            cudaMemcpy(odata, dev_odata, count * sizeof(int), cudaMemcpyDeviceToHost);
+            checkCUDAError("cudaMemcpy result to host failed!");
+
+            cudaFree(dev_idata);
+            cudaFree(dev_bools);
+            cudaFree(dev_indices);
+            cudaFree(dev_odata);
+
             timer().endGpuTimer();
-            return -1;
+            return count;
         }
     }
 }

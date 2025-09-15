3785c35b969441cd2222ac4f17c6e4a08d380000
Guan <harryg1@kite.upenn.edu>
2025-09-14 22:39:31 -0400

    feat: did thrust

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -22,6 +22,15 @@ namespace StreamCompaction {
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
             // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+
+            thrust::host_vector<int> h_in(idata, idata + n);
+            thrust::device_vector<int> dv_in = h_in;
+            thrust::device_vector<int> dv_out(n);
+            thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            thrust::host_vector<int> h_out = dv_out;
+            for (int i = 0; i < n; i++) {
+                odata[i] = h_out[i];
+            }
             timer().endGpuTimer();
         }
     }
